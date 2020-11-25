2e876225c2c325714a047ed77abfc5b4dd6f09d3
IvanPaez <ivan.paez@adlinktech.com>
2020-11-25 14:11:20 +0100

    added zenoh-pico .deb script

---
+++ howto/run_zenoh-pico_deb.sh
@@ -0,0 +1,48 @@
+
+# Script to generate the debs pakages for zenohc-dev
+
+VERSION="0.5.0"
+MAINDIR="libzenohpico"
+
+echo "  Creating libzenohpico deb structure .."
+
+# create deb structure for the deb package
+
+mkdir -p $MAINDIR-$VERSION/DEBIAN
+mkdir -p $MAINDIR-$VERSION/usr/local/lib
+mkdir -p $MAINDIR-$VERSION/usr/local/include
+
+ echo "  Creating control file ..."
+
+  echo "Package: $MAINDIR " >> control
+  echo "Version: $VERSION " >> control
+  echo "Section: custom " >> control
+  echo "Priority: optional" >> control
+  echo "Architecture: all" >> control
+  echo "Essential: no" >> control
+  echo "Installed-Size: 1024" >> control
+  echo "Depends: libev4" >> control
+  echo "Maintainer: ATO Team geeks@adlink-labs.tech" >> control
+  echo "Description: $MAINDIR deb package" >> control
+
+  mv control $MAINDIR-$VERSION/DEBIAN/
+
+echo "  Copying headers and binaries files ..."
+
+# i need to do a git clone and then look for the includes folder
+
+git clone -b dev git@github.com:eclipse-zenoh/zenoh-pico.git
+cp -r zenoh-pico/include $MAINDIR-$VERSION/usr/local/include
+
+cd zenoh-pico
+make
+cp ./build/libzenohpico.so ../$MAINDIR-$VERSION/usr/local/lib
+cd ../
+
+echo "  Creating the .deb package ... "
+
+dpkg -b ./$MAINDIR-$VERSION ./libzenohcpico.deb
+
+rm -rf zenoh-pico
+rm -rf ./$MAINDIR-$VERSION
+
